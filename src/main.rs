cefb650d81293b3e4f8678e183298c09c73fd4f6
Maginis00 <hemesothj@gmail.com>
2025-05-01 15:34:48 +0200

    Added small container for image display for phone

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,15 @@ mod phone;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+              primary_window: Some(Window {         
+                    title: "The Static Feed".to_string(),
+                    ..default()
+                }),
+                ..default()  
+            })
+        )
         .init_state::<states::GameStates>()
         .add_plugins(player::PlayerPlugin)
         .add_plugins(phone::PhonePlugin)
